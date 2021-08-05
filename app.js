10e7204af9da2afabd93cae95c3342770363fe06
Slava79198015981 <slava79198015981@gmail.com>
2021-08-05 22:49:29 +0300

    Прописанный функционал проекта

---
+++ app.js
@@ -0,0 +1,33 @@
+﻿const App = {
+    data() {
+        return {
+            placeholderString: "Введите слова(ограничение в 10 символов)",
+            title: "список заметок",
+            inputValue: "",
+            notes: [],
+        }
+    },
+    methods: {
+        addNewNote() {
+            if (this.inputValue !=='') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNode(idx) {
+            this.notes.splice(idx, 1)
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
+        },
+    }
+}
+
+Vue.createApp(App).mount('#app')
